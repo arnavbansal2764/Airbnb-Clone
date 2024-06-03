a3dd505dd380c171443fd31a36546d7821bc0ab6
Arnav Bansal <70932975+arnavbansal2764@users.noreply.github.com>
2024-06-03 15:25:53 +0530

    Fetch Reservations and display them

--- airbnb-clone/app/page.tsx
+++ airbnb-clone/app/page.tsx
@@ -9,24 +9,24 @@ export default async function Home() {
 
   const listings = await getListings();
   const currentUser = await getCurrentUser();
-  if(listings.length==0){
-    return(
+  if (listings.length == 0) {
+    return (
       <ClientOnly>
-        <EmptyState showReset/>
+        <EmptyState showReset />
       </ClientOnly>
     )
   }
-  
+
   return (
     <ClientOnly>
       <Container>
         <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-          {listings.map((listing:any)=>{
-            return(
+          {listings.map((listing) => {
+            return (
               <ListingCard
-                currentUser= {currentUser}
-                key = {listing.id}
-                data = {listing}
+                currentUser={currentUser}
+                key={listing.id}
+                data={listing}
               />
             )
           })}
